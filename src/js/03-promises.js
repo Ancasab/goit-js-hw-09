6efa57458dbdccfb959b61cb02ba922b662c2658
Anca_Sab <asabadeanu@gmail.com>
2024-08-18 21:31:44 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -18,21 +18,22 @@ function createPromise(position, delay) {
 }
 
 document.querySelector('.form').addEventListener("submit", event => {
-  event.preventDefault()
+  event.preventDefault();
   const delay = parseInt(event.target.delay.value);
   const step = parseInt(event.target.step.value);
   const amount = parseInt(event.target.amount.value);
 
-  for (let i = 0; i < amount; i++) {
-  const currentDelay = delay + step * i
-    createPromise(i + 1, currentDelay)
-      .then(value => { Notiflix.Notify.success(`✅ Fulfilled promise ${value.position} in ${value.delay}ms`) })
-    
-      .catch(error => { Notiflix.Notify.failure(`❌ Rejected promise ${error.position} in ${error.delay}ms`)})
+  for (let i = 1; i <= amount; i++) {
+    const currentDelay = delay + step * (i - 1); 
+    createPromise(i, currentDelay)
+      .then(value => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${value.position} in ${value.delay}ms`);
+      })
+      .catch(error => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${error.delay}ms`);
+      });
   }
-})
-
-
+});
 
 
 
